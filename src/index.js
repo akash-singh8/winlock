47d33f8c5ec279d82c1a84978a78b29097f36491
Akash Singh <developer.akash8@gmail.com>
2024-11-14 00:48:27 +0530

    windows: add option to context menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
+const { app, BrowserWindow } = require("electron");
+const sudo = require("sudo-prompt");
+const path = require("node:path");
+const fs = require("node:fs");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
+}
+
+// Location of a flag file to check if context menu option is added
+const setupFilePath = path.join(app.getPath("userData"), "setup_complete.txt");
+
+function addContextMenuOption() {
+  const command = `
+      reg add "HKCU\\Software\\Classes\\Directory\\shell\\ProtectWithPassword" /ve /d "Protect with Password" /f
+    `;
+
+  sudo.exec(command, { name: "winlock" }, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error adding context menu: ${error}`);
+      return;
+    }
+    if (stderr) {
+      console.warn(`Warning: ${stderr}`);
+    }
+
+    // Create a setup file to avoid repeating setup on subsequent launches
+    fs.writeFileSync(setupFilePath, "Context menu setup complete.");
+    console.log("Context menu option added successfully! \n stdout :", stdout);
+  });
 }
 
 const createWindow = () => {
@@ -12,40 +37,21 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
-
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
+app.on("ready", () => {
+  if (!fs.existsSync(setupFilePath)) {
+    addContextMenuOption();
   }
-});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+  createWindow();
+});
