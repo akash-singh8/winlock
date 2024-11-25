7d77c83a05c23f371a0751c8dd81f37bb9206da7
Akash Singh <developer.akash8@gmail.com>
2024-11-25 17:13:54 +0530

    preload: expose event handlers

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,9 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  onEvent: (name, handler) => ipcRenderer.on(name, handler),
+  sendMessage: (channel, message) => ipcRenderer.send(channel, message),
+});
